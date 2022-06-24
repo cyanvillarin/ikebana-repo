0fade17135649906fcfe6b209f0bf90b86bccce9
Cyan Villarin <cyanvillarin@gmail.com>
2022-06-24 17:50:58 +0900

    Added Styles Details View

---
+++ Ikebana/Materials/MaterialsDetailViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MaterialsDetailViewController.swift
+//  Ikebana
+//
+//  Created by CyanCamit.Villari on 2022/06/24.
+//
+
+import Foundation
+import UIKit
+
+class MaterialsDetailViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
---
+++ Ikebana/Styles/StylesDetailViewController.swift
@@ -0,0 +1,17 @@
+//
+//  StylesDetailViewController.swift
+//  Ikebana
+//
+//  Created by CyanCamit.Villari on 2022/06/24.
+//
+
+import Foundation
+import UIKit
+
+class StylesDetailViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
