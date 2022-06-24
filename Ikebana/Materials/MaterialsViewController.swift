ff590441792f057472901642238ed45d9c27e189
Cyan Villarin <cyanvillarin@gmail.com>
2022-06-24 18:18:20 +0900

    Updated UI for the Details screens for Styles and Materials

--- Ikebana/Materials/MaterialsViewController.swift
+++ Ikebana/Materials/MaterialsViewController.swift
@@ -10,75 +10,79 @@ import UIKit
 import SwiftUI
 
 class MaterialsViewController: BaseViewController {
-   
-   let adUnitId = "ca-app-pub-8855918227783661/1322009664"
-   
-   @IBOutlet var tableView: UITableView!
-   
-   let cellHeight: CGFloat = 87.0
-   
-   let materials: [Material] = [
-      Material(japaneseName: "Take 竹", englishName: "Bamboo", imageName: "take"),
-      Material(japaneseName: "Sasa 笹", englishName: "Bamboo Grass", imageName: "sasa"),
-      Material(japaneseName: "Tsuru Ume Modoki 鶴梅擬", englishName: "Bittersweet", imageName: "tsuruumemodoki"),
-      Material(japaneseName: "Tsubaki 椿", englishName: "Camellia", imageName: "tsubaki"),
-      Material(japaneseName: "Senryo 千両", englishName: "Ardisia", imageName: "senryo"),
-      Material(japaneseName: "Kiku 菊", englishName: "Chrysamthemum", imageName: "kiku"),
-      Material(japaneseName: "Shaga 著莪", englishName: "Iris", imageName: "shobu"),
-      Material(japaneseName: "Mikan みかん", englishName: "Orang", imageName: "mikan"),
-      Material(japaneseName: "Ume 梅", englishName: "Plum / Apricot", imageName: "ume"),
-      Material(japaneseName: "Boke 木瓜", englishName: "Quince", imageName: "boke"),
-      Material(japaneseName: "Nanten 南天", englishName: "Nandina / Nandida", imageName: "nanten"),
-      Material(japaneseName: "Suisen 水仙", englishName: "Narcissus", imageName: "suisen"),
-      Material(japaneseName: "Momo 桃", englishName: "Peach", imageName: "momo"),
-      Material(japaneseName: "Matsu 松", englishName: "Pine Tree", imageName: "matsu"),
-      Material(japaneseName: "Omoto 万年青", englishName: "Rohdea", imageName: "omoto"),
-      Material(japaneseName: "Kanchiku 寒竹", englishName: "Non-hollow Bamboo", imageName: "kanchiku"),
-      Material(japaneseName: "Botan 牡丹", englishName: "Tree Peony", imageName: "botan"),
-      Material(japaneseName: "Shidare Yanagi 枝垂れ柳", englishName: "Weeping Willow", imageName: "shidareyanagi"),
-      Material(japaneseName: "Ume Modoki 梅擬", englishName: "Winterberry", imageName: "umemodoki"),
-      Material(japaneseName: "Roubai 蝋梅", englishName: "Wintersweet", imageName: "roubai")
-   ]
-   
-   let imageNames = [""]
-   let japaneseNames = [""]
-   let englishNames = [""]
-   
-   override func viewDidLoad() {
-      super.viewDidLoad()
-      
-      tableView.delegate = self
-      tableView.dataSource = self
-      tableView.separatorStyle = .none
-      tableView.register(UINib(nibName: "MaterialsTableViewCell", bundle: nil), forCellReuseIdentifier: "MaterialsTableViewCell")
-      
-       self.navigationController?.navigationBar.prefersLargeTitles = true
-   }
-
+    
+    let adUnitId = "ca-app-pub-8855918227783661/1322009664"
+    
+    @IBOutlet var tableView: UITableView!
+    
+    let cellHeight: CGFloat = 87.0
+    
+    let materials: [Material] = [
+        Material(japaneseName: "Take 竹", englishName: "Bamboo", imageName: "take"),
+        Material(japaneseName: "Sasa 笹", englishName: "Bamboo Grass", imageName: "sasa"),
+        Material(japaneseName: "Tsuru Ume Modoki 鶴梅擬", englishName: "Bittersweet", imageName: "tsuruumemodoki"),
+        Material(japaneseName: "Tsubaki 椿", englishName: "Camellia", imageName: "tsubaki"),
+        Material(japaneseName: "Senryo 千両", englishName: "Ardisia", imageName: "senryo"),
+        Material(japaneseName: "Kiku 菊", englishName: "Chrysamthemum", imageName: "kiku"),
+        Material(japaneseName: "Shaga 著莪", englishName: "Iris", imageName: "shobu"),
+        Material(japaneseName: "Mikan みかん", englishName: "Orang", imageName: "mikan"),
+        Material(japaneseName: "Ume 梅", englishName: "Plum / Apricot", imageName: "ume"),
+        Material(japaneseName: "Boke 木瓜", englishName: "Quince", imageName: "boke"),
+        Material(japaneseName: "Nanten 南天", englishName: "Nandina / Nandida", imageName: "nanten"),
+        Material(japaneseName: "Suisen 水仙", englishName: "Narcissus", imageName: "suisen"),
+        Material(japaneseName: "Momo 桃", englishName: "Peach", imageName: "momo"),
+        Material(japaneseName: "Matsu 松", englishName: "Pine Tree", imageName: "matsu"),
+        Material(japaneseName: "Omoto 万年青", englishName: "Rohdea", imageName: "omoto"),
+        Material(japaneseName: "Kanchiku 寒竹", englishName: "Non-hollow Bamboo", imageName: "kanchiku"),
+        Material(japaneseName: "Botan 牡丹", englishName: "Tree Peony", imageName: "botan"),
+        Material(japaneseName: "Shidare Yanagi 枝垂れ柳", englishName: "Weeping Willow", imageName: "shidareyanagi"),
+        Material(japaneseName: "Ume Modoki 梅擬", englishName: "Winterberry", imageName: "umemodoki"),
+        Material(japaneseName: "Roubai 蝋梅", englishName: "Wintersweet", imageName: "roubai")
+    ]
+    
+    let imageNames = [""]
+    let japaneseNames = [""]
+    let englishNames = [""]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.separatorStyle = .none
+        tableView.register(UINib(nibName: "MaterialsTableViewCell", bundle: nil), forCellReuseIdentifier: "MaterialsTableViewCell")
+        
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
 }
 
 extension MaterialsViewController: UITableViewDelegate {
-   func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-      print(indexPath.row)
-   }
-   
-   func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-      return cellHeight
-   }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "MaterialsDetailViewController") as? MaterialsDetailViewController,
+           let navigator = navigationController {
+            self.navigationController?.navigationBar.prefersLargeTitles = false
+            navigator.pushViewController(vc, animated: true)
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return cellHeight
+    }
 }
 
 extension MaterialsViewController: UITableViewDataSource {
-   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-      let cell = tableView.dequeueReusableCell(withIdentifier: "MaterialsTableViewCell", for: indexPath) as! MaterialsTableViewCell
-      DispatchQueue.main.async {
-         cell.setup(material: self.materials[indexPath.row])
-         cell.selectionStyle = .none
-      }
-      return cell
-   }
-   
-   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-      return 20
-   }
-   
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MaterialsTableViewCell", for: indexPath) as! MaterialsTableViewCell
+        DispatchQueue.main.async {
+            cell.setup(material: self.materials[indexPath.row])
+            cell.selectionStyle = .none
+        }
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
 }
--- Ikebana/Styles/StylesViewController.swift
+++ Ikebana/Styles/StylesViewController.swift
@@ -11,120 +11,124 @@ import AppTrackingTransparency
 import AdSupport
 
 class StylesViewController: BaseViewController {
-   
-   let itemWidth: CGFloat = 100.0
-   
-   let styles: [Style] = [
-      Style(name: "Rikka 立花", imageName: "rikka"),
-      Style(name: "Seika 活花", imageName: "seika"),
-      Style(name: "Moribana 盛花", imageName: "moribana"),
-      Style(name: "Modern Ikebana 現代生花", imageName: "contemporary-ikebana")
-   ]
-   
-   private weak var _carousel : CircularCarousel!
-   @IBOutlet var carousel : CircularCarousel! {
-      set {
-         _carousel = newValue
-         _carousel.delegate = self
-         _carousel.dataSource = self
-      }
-      get {
-         return _carousel
-      }
-   }
-   
-   @IBOutlet var backgroundImage: UIImageView!
-   @IBOutlet var styleNameLabel: UILabel!
-   
-   override func viewDidLoad() {
-      super.viewDidLoad()
-      backgroundImage.applyBlurEffect()
-      carousel.backgroundColor = .clear
-      
-      DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
-         if #available(iOS 14, *) {
-            ATTrackingManager.requestTrackingAuthorization(completionHandler: { status in
-               // Tracking authorization completed. Start loading ads here.
-            })
-         }
-      }
-   }
-   
-   override var preferredStatusBarStyle: UIStatusBarStyle {
-      return .default
-   }
-   
+    
+    let itemWidth: CGFloat = 100.0
+    
+    let styles: [Style] = [
+        Style(name: "Rikka 立花", imageName: "rikka"),
+        Style(name: "Seika 活花", imageName: "seika"),
+        Style(name: "Moribana 盛花", imageName: "moribana"),
+        Style(name: "Modern Ikebana 現代生花", imageName: "contemporary-ikebana")
+    ]
+    
+    private weak var _carousel : CircularCarousel!
+    @IBOutlet var carousel : CircularCarousel! {
+        set {
+            _carousel = newValue
+            _carousel.delegate = self
+            _carousel.dataSource = self
+        }
+        get {
+            return _carousel
+        }
+    }
+    
+    @IBOutlet var backgroundImage: UIImageView!
+    @IBOutlet var styleNameLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        backgroundImage.applyBlurEffect()
+        carousel.backgroundColor = .clear
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            if #available(iOS 14, *) {
+                ATTrackingManager.requestTrackingAuthorization(completionHandler: { status in
+                    // Tracking authorization completed. Start loading ads here.
+                })
+            }
+        }
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .default
+    }
+    
 }
 
 extension StylesViewController: CircularCarouselDataSource {
-   func numberOfItems(inCarousel carousel: CircularCarousel) -> Int {
-      return styles.count
-   }
-   
-   func carousel(_: CircularCarousel, viewForItemAt indexPath: IndexPath, reuseView view: UIView?) -> UIView {
-      var view = view
-      
-      if view == nil {
-         view = UIView(frame: CGRect(x: 0, y: 0, width: self.view.bounds.width, height: 300))
-         view?.backgroundColor = .clear
-         let imageView = UIImageView()
-         imageView.frame = CGRect(x: 0, y: 0, width: self.view.bounds.width, height: 300)
-         if let imageName = styles[indexPath.row].imageName {
-            if let image = UIImage(named: imageName) {
-               imageView.image = image
-               imageView.clipsToBounds = true
-               imageView.contentMode = .scaleAspectFit
-               imageView.backgroundColor = .clear
+    func numberOfItems(inCarousel carousel: CircularCarousel) -> Int {
+        return styles.count
+    }
+    
+    func carousel(_: CircularCarousel, viewForItemAt indexPath: IndexPath, reuseView view: UIView?) -> UIView {
+        var view = view
+        
+        if view == nil {
+            view = UIView(frame: CGRect(x: 0, y: 0, width: self.view.bounds.width, height: 300))
+            view?.backgroundColor = .clear
+            let imageView = UIImageView()
+            imageView.frame = CGRect(x: 0, y: 0, width: self.view.bounds.width, height: 300)
+            if let imageName = styles[indexPath.row].imageName {
+                if let image = UIImage(named: imageName) {
+                    imageView.image = image
+                    imageView.clipsToBounds = true
+                    imageView.contentMode = .scaleAspectFit
+                    imageView.backgroundColor = .clear
+                }
             }
-         }
-         view?.addSubview(imageView)
-      }
-      
-      return view!
-   }
-   
-   func startingItemIndex(inCarousel carousel: CircularCarousel) -> Int {
-      return 0
-   }
+            view?.addSubview(imageView)
+        }
+        
+        return view!
+    }
+    
+    func startingItemIndex(inCarousel carousel: CircularCarousel) -> Int {
+        return 0
+    }
 }
 
 extension StylesViewController: CircularCarouselDelegate {
-   func carousel<CGFloat>(_ carousel: CircularCarousel, valueForOption option: CircularCarouselOption, withDefaultValue defaultValue: CGFloat) -> CGFloat {
-      switch option {
-      case .itemWidth:
-         return itemWidth as! CGFloat
-         /*  Insert one of the following handlers :
-          case spacing
-          case fadeMin
-          case fadeMax
-          case fadeRange
-          case fadeMinAlpha
-          case offsetMultiplier
-          case itemWidth
-          case scaleMultiplier
-          case minScale
-          case maxScale
-          */
-      default:
-         return defaultValue
-      }
-   }
-   func carousel(_ carousel: CircularCarousel, didSelectItemAtIndex index: Int) {
-      /* Handle selection of the selected carousel item */
-   }
-   func carousel(_ carousel: CircularCarousel, willBeginScrollingToIndex index: Int) {
-      if let imageName = styles[index].imageName {
-         if let image = UIImage(named: imageName) {
-            backgroundImage.image = image
-            backgroundImage.contentMode = .scaleAspectFill
-         }
-      }
-      if let styleLabel = styleNameLabel {
-         styleLabel.text = styles[index].name
-         styleLabel.alpha = 0.6
-      }
-   }
-   func carousel(_ carousel: CircularCarousel, spacingForOffset offset: CGFloat) -> CGFloat {
-      return 10.0
-   }
+    func carousel<CGFloat>(_ carousel: CircularCarousel, valueForOption option: CircularCarouselOption, withDefaultValue defaultValue: CGFloat) -> CGFloat {
+        switch option {
+        case .itemWidth:
+            return itemWidth as! CGFloat
+            /*  Insert one of the following handlers :
+             case spacing
+             case fadeMin
+             case fadeMax
+             case fadeRange
+             case fadeMinAlpha
+             case offsetMultiplier
+             case itemWidth
+             case scaleMultiplier
+             case minScale
+             case maxScale
+             */
+        default:
+            return defaultValue
+        }
+    }
+    
+    func carousel(_ carousel: CircularCarousel, didSelectItemAtIndex index: Int) {
+        if let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "StylesDetailViewController") as? StylesDetailViewController {
+            self.present(vc, animated: true)
+        }
+    }
+    
+    func carousel(_ carousel: CircularCarousel, willBeginScrollingToIndex index: Int) {
+        if let imageName = styles[index].imageName {
+            if let image = UIImage(named: imageName) {
+                backgroundImage.image = image
+                backgroundImage.contentMode = .scaleAspectFill
+            }
+        }
+        if let styleLabel = styleNameLabel {
+            styleLabel.text = styles[index].name
+            styleLabel.alpha = 0.6
+        }
+    }
+    func carousel(_ carousel: CircularCarousel, spacingForOffset offset: CGFloat) -> CGFloat {
+        return 10.0
+    }
 }
